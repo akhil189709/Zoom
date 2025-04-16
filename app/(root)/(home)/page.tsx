ae5a8f50a9fd07936efa1cbfa9989dcc973239e5
Akhil Dhiman <akhildhiman1897@gmail.com>
2025-04-17 03:31:01 +0530

    changes in home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,8 +3,14 @@ import MeetingTypeList from '@/components/MeetingTypeList';
 const Home = () => {
   const now = new Date();
 
-  const time = now.toLocaleTimeString('en-IN', { hour: '2-digit', minute: '2-digit' });
-  const date = (new Intl.DateTimeFormat('en-IN', { dateStyle: 'full' })).format(now);
+  const time = now.toLocaleTimeString('en-IN', {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+
+  const date = new Intl.DateTimeFormat('en-IN', {
+    dateStyle: 'full',
+  }).format(now);
 
   return (
     <section className="flex size-full flex-col gap-5 text-white">
@@ -13,7 +19,9 @@ const Home = () => {
           <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
           </h2>
           <div className="flex flex-col gap-2">
-            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
+            <h1 className="text-4xl font-extrabold lg:text-7xl">
+              {time} <span className="text-base font-semibold">IST</span>
+            </h1>
             <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
           </div>
         </div>
@@ -25,3 +33,4 @@ const Home = () => {
 };
 
 export default Home;
+
